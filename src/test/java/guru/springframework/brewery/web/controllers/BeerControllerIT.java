aff9545fbd1c3578589dd8bfb440b4ac7c6f1a04
Nick Gorajski <nick_gorajski@bcbsil.com>
2020-02-05 19:59:14 -0600

    Demo RestTemplate and WebEnvironment to get full Spring env for Integration Testing

---
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerIT.java
@@ -0,0 +1,27 @@
+package guru.springframework.brewery.web.controllers;
+
+import guru.springframework.brewery.web.model.BeerPagedList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//All Beans created and wired, bootstrap is loaded, H2 database, repositories injected.
+//Use webEnvironment for integration tests.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BeerControllerIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testListBeers() {
+        BeerPagedList beerPagedList = restTemplate.getForObject("/api/v1/beer", BeerPagedList.class);
+
+        assertThat(beerPagedList.getContent()).hasSize(3);
+
+
+    }
+}
